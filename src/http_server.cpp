dd44b78a57fc1c653758e939c9db106c4b84d0c9
Omid Rostamabadi <omidrostamabadi@gmail.com>
2022-07-30 20:51:36 +0200

    Add file response to http server

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -37,15 +37,11 @@ namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-// cv::Mat get_current_frame() {
-//   cv::VideoCapture cam(0);
-//   if(!cam.isOpened()) {
-//     std::cerr << __FILE__ << ":" << __LINE__ << " :Could not open camera\n";
-//   }
-//   cv::Mat ret_mat;
-//   cam >> ret_mat;
-//   return ret_mat;
-// }
+/* Working directory of the server */
+std::string wd = "/home/punisher/Documents/courses/Embsys/project/build/";
+
+/* The directory to save temporary photos at. It is relative to wd */
+std::string photo_path = "photo/";
 
 namespace my_program_state
 {
@@ -69,6 +65,7 @@ public:
     http_connection(tcp::socket socket)
         : socket_(std::move(socket))
     {
+      is_file = false;
     }
 
     // Initiate the asynchronous operations associated with the connection.
@@ -91,6 +88,10 @@ private:
 
     // The response message.
     http::response<http::dynamic_body> response_;
+
+    http::response<http::file_body> file_response_;
+
+    bool is_file;
 
     // The timer for putting a deadline on connection processing.
     net::steady_timer deadline_{
@@ -145,26 +146,91 @@ private:
         write_response();
     }
 
+
+    // void send_file(beast::string_view target)
+    // {
+    //     // Request path must be absolute and not contain "..".
+    //     if (target.empty() || target[0] != '/' || target.find("..") != std::string::npos)
+    //     {
+    //         send_bad_response(
+    //             http::status::not_found,
+    //             "File not found\r\n");
+    //         return;
+    //     }
+
+    //     std::string full_path = doc_root_;
+    //     full_path.append(
+    //         target.data(),
+    //         target.size());
+
+    //     http::file_body::value_type file;
+    //     beast::error_code ec;
+    //     file.open(
+    //         full_path.c_str(),
+    //         beast::file_mode::read,
+    //         ec);
+    //     if(ec)
+    //     {
+    //         send_bad_response(
+    //             http::status::not_found,
+    //             "File not found\r\n");
+    //         return;
+    //     }
+
+    //     file_response_.emplace(
+    //         std::piecewise_construct,
+    //         std::make_tuple(),
+    //         std::make_tuple(alloc_));
+
+    //     file_response_->result(http::status::ok);
+    //     file_response_->keep_alive(false);
+    //     file_response_->set(http::field::server, "Beast");
+    //     file_response_->set(http::field::content_type, mime_type(std::string(target)));
+    //     file_response_->body() = std::move(file);
+    //     file_response_->prepare_payload();
+
+    //     file_serializer_.emplace(*file_response_);
+
+    //     http::async_write(
+    //         socket_,
+    //         *file_serializer_,
+    //         [this](beast::error_code ec, std::size_t)
+    //         {
+    //             socket_.shutdown(tcp::socket::shutdown_send, ec);
+    //             file_serializer_.reset();
+    //             file_response_.reset();
+    //             accept();
+    //         });
+    // }
+
     // Construct a response message based on the program state.
     void
     create_response()
     {
+      is_file = false;
+      static cv::Mat curr_frame;
+      std::string file_name = "test.png";
+      std::string file_abs_path = wd + photo_path + file_name;
+      beast::error_code ec;
         if(request_.target() == "/capture-photo")
         {
-            response_.set(http::field::content_type, "text/html");
-            beast::ostream(response_.body())
-                << "<html>\n"
-                <<  "<head><title>Capture Photo</title></head>\n"
-                <<  "<body>\n"
-                <<  "<h1>Photo</h1>\n"
-                <<  "<p>There have been "
-                <<  my_program_state::request_count()
-                <<  " requests so far.</p>\n"
-                <<  "</body>\n"
-                <<  "</html>\n";
-            cv::Mat curr_frame = get_current_frame();
-            cv::imwrite("test.png", curr_frame);
-            std::cout << "Saved the image\n";
+          curr_frame = get_current_frame();
+          cv::imwrite(file_abs_path, curr_frame);
+          LOG("Photo saved", std::cout, "HTTP SERVER")
+          response_.set(http::field::content_type, "text/html");
+          beast::ostream(response_.body())
+              << "<html>\n"
+              <<  "<head><title>Capture Photo</title></head>\n"
+              <<  "<body>\n"
+              <<  "<h1>Photo</h1>\n"
+              <<  "<p>There have been "
+              <<  my_program_state::request_count()
+              <<  " requests so far.</p>\n"
+              << "<a href=\""
+              << photo_path + file_name
+              << "\">see</a>"
+              <<  "</body>\n"
+              <<  "</html>\n";
         }
         else if(request_.target() == "/time")
         {
@@ -182,9 +248,20 @@ private:
         }
         else
         {
-            response_.result(http::status::not_found);
-            response_.set(http::field::content_type, "text/plain");
-            beast::ostream(response_.body()) << "File not found\r\n";
+          LOG("Sending file", std::cout, "HTTP SERVER")
+          is_file = true;
+          http::file_body::value_type file;
+          file.open((photo_path + file_name).c_str(), beast::file_mode::read, ec);
+          file_response_.result(http::status::ok);
+          file_response_.keep_alive(false);
+          file_response_.set(http::field::server, "Beast");
+          file_response_.set(http::field::content_type, "image/png");
+          file_response_.body() = std::move(file);
+          file_response_.prepare_payload();
+          // response_.result(http::status::not_found);
+          // response_.set(http::field::content_type, "image/*");
+          // // beast::ostream(response_.body()) << "File not found\r\n";
+          // beast::ostream(response_.body()) << curr_frame << "\r";
         }
     }
 
@@ -193,17 +270,30 @@ private:
     write_response()
     {
         auto self = shared_from_this();
+        if(is_file) {
+          file_response_.set(http::field::content_length, file_response_.body().size());
 
-        response_.set(http::field::content_length, response_.body().size());
+          http::async_write(
+              socket_,
+              file_response_,
+              [self](beast::error_code ec, std::size_t)
+              {
+                  self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+                  self->deadline_.cancel();
+              });
+        }
+        else {
+          response_.set(http::field::content_length, response_.body().size());
 
-        http::async_write(
-            socket_,
-            response_,
-            [self](beast::error_code ec, std::size_t)
-            {
-                self->socket_.shutdown(tcp::socket::shutdown_send, ec);
-                self->deadline_.cancel();
-            });
+          http::async_write(
+              socket_,
+              response_,
+              [self](beast::error_code ec, std::size_t)
+              {
+                  self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+                  self->deadline_.cancel();
+              });
+        }
     }
 
     // CHECK_VOID whether we have spent enough time on this connection.
@@ -241,6 +331,15 @@ void http_server(int argc, char* argv[])
 {
     try
     {
+      if(mkdir((wd + photo_path).c_str(), S_IRWXU | S_IRWXG | S_IRWXO | S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH | S_IWOTH)) {
+        if(errno != EEXIST) {
+          std::cerr << "Error in mkdir: " << errno << std::endl;
+          return;
+        }
+        else {
+          LOG("Photo directory already exists", std::cout, "HTTP SERVER")
+        }
+      }
       CHECK_VOID(init_camera(), "Cannot init camera", std::cerr)
         // check command line arguments.
         if(argc != 3)
